70741016741e03267c1b5c2a8caea0f018a686fe
Shaganashree P L <shaganashree.pl@gmail.com>
2025-04-16 20:38:16 +0530

    Fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()]
-    }
-  },
-  resolve: {
-    alias: {
-      crypto: 'rollup-plugin-node-polyfills/polyfills/crypto-browserify',
-      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-    }
-  },
-  define: {
-    'process.env': {}
-  }
+  base: '/personal-finance-tracker-frontend/',
 })
