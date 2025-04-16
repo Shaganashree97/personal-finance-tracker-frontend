7ec5ae4d62f274627e28a78f48327c3a7de5a2d8
Shaganashree P L <shaganashree.pl@gmail.com>
2025-04-16 20:13:20 +0530

    trying to solve error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()]
+    }
+  },
+  resolve: {
+    alias: {
+      crypto: 'rollup-plugin-node-polyfills/polyfills/crypto-browserify',
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+    }
+  },
+  define: {
+    'process.env': {}
+  }
 })
